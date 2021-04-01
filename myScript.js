8f220d32c271cf1c66229ade5a5000920b178bd8
adyhariri <mahmoudalhariri1994@gmail.com>
2021-04-01 16:12:07 +0300

    doing some edit on first js opject

--- myScript.js
+++ myScript.js
@@ -2,5 +2,14 @@ alert('hello and welcome to my page');
 var fName = prompt("what is your name ?","inter your name here");
 var major = prompt("what is your major?","like civil engineering");
 var state = prompt("are you still student?","answer with yes/no");
+if (state == 'yes')
+{
+    alert('hope you will graduate soon');
+}
+else alert('great!');
 var ltuc = prompt("are you registering now with abdalaziez college scolarship ?","please answer with yes/no");
+if(ltuc=='yes'){
+    alert('hope you enjoy being there');
+}
+else alert('you should register with it');
 alert ('thank you for visiting my page i know now some informations about you like , your name is ' + fName + ',your major is ' + major + ', if you still student is ' + state + ' and if you rigester with scholarship is ' + ltuc);
