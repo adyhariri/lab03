ffffe3238c30f96b47ff07d8c3010fa39b23a002
adyhariri <mahmoudalhariri1994@gmail.com>
2021-04-01 16:00:59 +0300

    first java script project

---
+++ myScript.js
@@ -0,0 +1,6 @@
+alert('hello and welcome to my page');
+var fName = prompt("what is your name ?","inter your name here");
+var major = prompt("what is your major?","like civil engineering");
+var state = prompt("are you still student?","answer with yes/no");
+var ltuc = prompt("are you registering now with abdalaziez college scolarship ?","please answer with yes/no");
+alert ('thank you for visiting my page i know now some informations about you like , your name is ' + fName + ',your major is ' + major + ', if you still student is ' + state + ' and if you rigester with scholarship is ' + ltuc);
